35e84d5020df0c9b1415e73b60977e8097db1c69
Johann150 <Johann150@users.noreply.github.com>
2017-07-13 18:09:29 +0200

    Seitentitel geändert

--- not-found.php
+++ not-found.php
@@ -6,7 +6,7 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Willkommen - Handschriftenarchiv Dresdner Kreuzchor</title>
+	<title>404 - Handschriftenarchiv Dresdner Kreuzchor</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Offizielle Website des Handschriftenarchiv Dresdner Kreuzchor" />
 	<meta name="keywords" content="Handschrift, Kreuzchor, Geschichte, Archiv, Dresden" />
