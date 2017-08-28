3bea1207cd2ab05da92970ba1c7c48d3214cd53f
Johann150 <Johann150@users.noreply.github.com>
2017-08-28 22:05:08 +0200

    Update misc.php

--- suche/misc.php
+++ suche/misc.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once "config.php";
+$con=dbCon();
+
 function formatSig($sig){
 	$sig=str_pad($sig,6,'0',STR_PAD_LEFT);
 	return substr($sig,0,2).'-'.substr($sig,2,2).'-'.substr($sig,4);
