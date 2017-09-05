7a3be3df3e045b70901ed0c2ef7aec2f26791b95
Johann150 <Johann150@users.noreply.github.com>
2017-09-05 23:19:16 +0200

    Aktualisiert
    Entspricht jetzt neuestem Kenntsnisstand zu EAD(DDB)

--- suche/export.php
+++ suche/export.php
@@ -1,31 +1,118 @@
 <?php
 require_once "misc.php";
-header("Content-Type: text/plain");
-$ident="DE-HSA";
-$res=mysqli_query($con,"SELECT * FROM archivalien");
-while($dsatz=mysqli_fetch_assoc($res)){
-	$sig=formatSig($dsatz['Signatur']);
-	$f=fopen("export".DIRECTORY_SEPARATOR."$sig.xml","w");
-	fprintf($f,"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<ead xsi:schemaLocation=\"urn:isbn:1-931666-22-9 http://www.loc.gov/ead/ead.xsd http://www.w3.org/1999/xlink http://www.loc.gov/standards/xlink/xlink.xsd\">\n<eadheader>");
-	fprintf($f,"<eadid>%s</eadid>\n<filedesc>\n<titlestmt>\n<titleproper>%s</titleproper>\n</titlestmt>\n</filedesc>\n",$ident,$dsatz['Titel']);
-	$date=strtotime(preg_replace('~^[a-zA-Zäöüß.\s]*(.+)[a-zA-Zäöüß.\s]*$~', '$1', $dsatz['Verfassungsdatum']));
-	fprintf($f,"<profiledesc>\n<creation><date normal=\"%s\">%s</date>\n</creation>\n</profiledesc>\n</eadheader>\n",date('Y-m-d',$date),$dsatz['Verfassungsdatum']);
-	fprintf($f,"<archdesc level=\"collection\" type=\"Findbuch\">\n<did>\n<repository>\n<corpname role=\"Sonstige\">Handschriftenarchiv Dresdner Kreuzchor</corpname>\n");
-	fprintf($f,"<address><adressline>Dornblüthstraße 4,01277 Dresden,handschriftenarchiv@protonmail.com</adressline>\n<address>\n<extref xlink:role=\"url_archive\" xlink:href=\"archiv.handschriften.bplaced.com\">Handschriftenarchiv Dresdner Kreuzchor</extref>\n</repository>\n</did>\n");
-	fprintf($f,"<otherfindaid>\n<extref xlink:role=\"url_findbuch\" xlink:href=\"archiv.handschriften.bplaced.com/suche/details.php?id=%d\">%s im Katalog</extref>\n</otherfindaid>\n",$dsatz['ID'],$dsatz['Titel']);
-	fprintf($f,"<dsc>\n<c level=\"collection\" id=\"$sig\">\n<did>\n<unitid>$sig</unitid>\n<unittitle>%s</unittitle>\n",$dsatz['Titel']);
-	$norm_type="";
-	switch($dsatz['Typus']){
-		case 'Handschrift':$norm_type='Handschriften';break;
+header("Content-Type: text/xml");
+// Platzhalter für den Identifier
+$ident="DE-1234";
+?><?xml version="1.0" encoding="UTF-8"?>
+<ead xsi:schemaLocation="urn:isbn:1-931666-22-9 http://www.loc.gov/ead/ead.xsd http://www.w3.org/1999/xlink http://www.loc.gov/standards/xlink/xlink.xsd">
+	<eadheader>
+		<eadid mainagencycode="DE-ISIL"><?php echo $ident;?></eadid>
+		<filedesc>
+			<titlestmt>
+				<titleproper>Handschriftenarchiv Dresdner Kreuzchor</titleproper>
+			</titlestmt>
+		</filedesc>
+		<profiledesc>
+			<creation>
+				<date normal="<?php echo date('Y-m-d');?>">erstellt am <?php echo date('d.m.Y');?></date>
+			</creation>
+		</profiledesc>
+	</eadheader>
+	<archdesc level="collection" type="Findbuch">
+		<did>
+			<unitid>HSA-Bestand</unitid>
+			<repository>
+				<corpname role="Sonstige">Handschriftenarchiv Dresdner Kreuzchor</corpname>
+				<address>
+					<adressline>Dornblüthstraße 4,01277 Dresden,handschriftenarchiv@protonmail.com</adressline>
+				</address>
+				<extref xlink:role="url_archive" xlink:href="archiv.handschriften.bplaced.com">Website des Handschriftenarchiv Dresdner Kreuzchor</extref>
+			</repository>
+		</did>
+		<otherfindaid>
+			<extref xlink:role="url_findbuch" xlink:href="archiv.handschriften.bplaced.de/suche/export.php">Findbuch Handschriftenarchiv Dresdner Kreuzchor</extref>
+		</otherfindaid>
+		<dsc>
+			<c level="collection" id="HSA-Bestand">
+				<did>
+					<unitid>HSA-Bestand</unitid>
+					<!-- evtl. <origination>Dresdner Kreuzchor</origination> -->
+				</did>
+<?php
+				$res=mysqli_query($con,'SELECT * FROM archivalien');
+				while($dsatz=mysqli_fetch_assoc($res)){
+					$sig=formatSig($dsatz['Signatur']);
+?>				<c level="file" id="<?php echo $ident.'-'.$sig; ?>">
+					<did>
+						<unitid><?php echo $sig; ?></unitid>
+						<unittitle type="title"><?php echo $dsatz['Titel']; ?></unittitle>
+						<origination label="Autor"><?php echo $dsatz['Komponist']?></origination>
+						<physdesc>
+<?php
+$norm_type="";
+switch($dsatz['Typus']){
+	case 'Handschrift':$norm_type='Handschriften';break;
 /* ?! */case 'Kopie von Handschrift':$norm_type='Sonstige';break;
-		case 'Druck':$norm_type='Drucksachen';break;
-	}
-	fprintf($f,"<physdesc>\n<genreform normal=\"%s\">%s</genreform>\n</physdesc>\n<langmaterial>\n<language",$norm_type,$dsatz['Typus']);
-	if($dsatz['Sprache']=="Deutsch"){
-		fprintf($f," langcode=\"ger\"");
-	}
-	if($dsatz['Schrift']=="Latein"){
-		fprintf($f," scriptcode=\"Latn\"");
-	}
-	fprintf($f,">%s</language>\n</langmaterial>\n</did>\n</c>\n</dsc>\n</archdesc>\n</ead>",$dsatz['Sprache']);
+	case 'Druck':$norm_type='Drucksachen';break;
 }
+?>							<genreform normal="<?php echo $norm_type; ?>"><?php echo $dsatz['Typus']; ?></genreform>
+						</physdesc>
+						<langmaterial>
+							<!-- hier noch Erweiterung auf Normdaten! -->
+							<language><?php echo $dsatz['Sprache']; ?></language>
+						</langmaterial>
+<?php
+if(!empty($dsatz['Bemerkungen'])){
+?>						<note>
+							<p><?php echo $dsatz['Bemerkungen']?></p>
+						</note>
+<?php
+}
+?>					</did>
+					<otherfindaid>
+						<extref xlink:role="url_archivalunit" xlink:href="archiv.handschriften.bplaced.de/suche/details.php?id=<?php echo $dsatz['ID'];?>">Titelaufnahme im Angebot des Archivs</extref>
+					</otherfindaid>
+					<odd>
+						<head>Anzahl</head>
+						<p><?php echo $dsatz['Anzahl']; ?></p>
+					</odd>
+<?php
+if(!empty($dsatz['Dokumentlink'])){
+?>					<daogrp id="<?php echo $ident.'-'.$sig; ?>_digit">
+						<daodesc>
+							<list>
+<?php
+$docs=explode(' ',$dsatz['Dokumentlink']);
+foreach($docs as $doc){
+?>								<item>
+									<title>Scan <?php
+switch($dsatz['Typus']){
+	case 'Handschrift':
+		echo "der Handschrift";
+		break;
+	case 'Kopie von Handschrift':
+		echo "der Kopie der Handschrift";
+		break;
+	case 'Druck':
+		echo "des Druckes";
+		break;
+}									?></title>
+									<genreform>BILD</genreform>
+								</item>
+<?php
+}
+?>
+							</list>
+						</daodesc>
+<?php
+foreach($docs as $doc){
+?>						<daoloc xlink:role="externer_viewer" xlink:href="<?php echo $doc; ?>"/>
+<?php }
+?>					</daogrp>
+<?php }
+?>
+				</c>
+<?php } ?>	</c>
+		</dsc>
+	</archdesc>
+</ead>
