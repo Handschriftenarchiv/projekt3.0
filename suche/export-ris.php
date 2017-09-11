22f6adfaa0b68ee15541270aa22ba869c1963907
Johann150 <Johann150@users.noreply.github.com>
2017-09-11 08:34:51 +0200

    RIS abgeschlossen
    es kann jetzt die ganze Datenbank exportiert werden.

--- suche/export-ris.php
+++ suche/export-ris.php
@@ -1,19 +1,27 @@
 <?php
 require_once 'misc.php';
 header('Content-Type: application/x-research-info-systems');
-$sql="SELECT* FROM archivalien";
+$sql="SELECT * FROM archivalien";
 if(isset($_GET['id'])){
 	$sql.=" WHERE id=".intval($_GET['id']);
 	$res=mysqli_query($con,$sql);
 	$dsatz=mysqli_fetch_assoc($res);
 	@header('Content-Disposition: attachment; filename="'.formatSig($dsatz['Signatur']).'.ris"');
-?>TY  - MUSIC
-DB  - Handschriftenarchiv Dresdner Kreuzchor
-DP  - Handschriftenarchiv Dresdner Kreuzchor
-AN  - <?php
-	echo $dsatz['ID'];
-	echo "\nLB  - ".formatSig($dsatz['Signatur']);
-	echo "\nTI  - $dsatz[Titel]";
+	ris($dsatz);
+}else{
+	$res=mysqli_query($con,$sql);
+	@header('Content-Disposition: attachment; filename="HSA.ris"');
+	while($dsatz=mysqli_fetch_assoc($res)){
+		ris($dsatz);
+	}
+}
+
+function ris($dsatz){
+	echo 'TY  - MUSIC'
+		."\nDP  - Handschriftenarchiv Dresdner Kreuzchor"
+		."\nAN  - $dsatz[ID]"
+		."\nLB  - ".formatSig($dsatz['Signatur'])
+		."\nTI  - $dsatz[Titel]";
 	if(!empty($dsatz['Komponist'])){
 		echo "\nAU  - $dsatz[Komponist]";
 	}
@@ -39,10 +47,7 @@ AN  - <?php
 	if(!empty($dsatz['Bemerkungen'])){
 		echo "\nRN  - $dsatz[Bemerkungen]";
 	}
-	echo "\nUR  - http://$_SERVER[SERVER_NAME]$_SERVER[REQUEST_URI]";
-	echo "\nY2  - ".date('Y/m/d');
-	echo "\nER  - \n";
-}else{
-	$res=mysqli_query($con,$sql);
-	// TODO
+	echo "\nUR  - http://$_SERVER[SERVER_NAME]$_SERVER[REQUEST_URI]"
+		."\nY2  - ".date('Y/m/d')
+		."\nER  - \n";
 }
