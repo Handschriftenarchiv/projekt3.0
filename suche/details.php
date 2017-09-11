b3ff32b9b566a39ff28f396d21a69154a326d4ea
Johann150 <Johann150@users.noreply.github.com>
2017-09-11 08:44:09 +0200

    aufgeräumt

--- suche/details.php
+++ suche/details.php
@@ -224,7 +224,8 @@ $dsatz['Signatur']=formatSig($dsatz['Signatur']);
 									?>
 								</table>
 								<br>
-								<a href="javascript:history.back()"><p>zurück zum Suchergebnis</p></a><a href="."><p>neue Suche</p></a><br>
+								<a href="javascript:history.back()"><p>zurück zum Suchergebnis</p></a>
+								<a href="."><p>neue Suche</p></a><br>
 								<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />Dieses Werk ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Namensnennung - Weitergabe unter gleichen Bedingungen 4.0 International Lizenz</a>.
 							</div>
 						</div>
