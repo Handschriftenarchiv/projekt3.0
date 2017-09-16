0dc761c2c5654e13f5dc24435d1d7b21822605a9
Johann150 <Johann150@users.noreply.github.com>
2017-09-16 21:32:39 +0200

    Fehler korrigiert
    Es werden jetzt Uhrzeit und Datum richtig eingefügt

--- analytics.php
+++ analytics.php
@@ -1,12 +1,14 @@
 <?php
 include 'suche/misc.php';
 
+view();
+
 function view(){
 	$con=dbCon();
 	$ip=$_SERVER['REMOTE_ADDR'];
 	$geo=unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ip));
 	$country=$geo['geoplugin_countryName'];
 	$page=$_SERVER['SCRIPT_NAME'];
-	$sql="INSERT INTO analytics(`country`,`page`,`date`)VALUES('$country','$page',now())";
+	$sql="INSERT INTO analytics(`country`,`page`,`date`,`time`)VALUES('$country','$page',CURRENT_DATE,CURRENT_TIME)";
 	mysqli_query($con,$sql);
 }
