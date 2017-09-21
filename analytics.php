44c4e650a138a14c47cba30594a836f1aa0fbbe6
Johann150 <Johann150@users.noreply.github.com>
2017-09-21 13:19:18 +0200

    Auswertung überarbeitet
    sollte #56 lösen

--- analytics.php
+++ analytics.php
@@ -7,15 +7,14 @@ function view(){
 	$con=dbCon();
 	$ip=$_SERVER['REMOTE_ADDR'];
 	$page=$_SERVER['SCRIPT_NAME'];
-	$sql="INSERT INTO analytics(`page`)VALUES('','$page')";
+	$sql="INSERT INTO analytics(`page`)VALUES('$page')";
 	mysqli_query($con,$sql);
 	mysqli_close($con);
 }
 
-function getPages($interval="1 MONTH"){
+function getPages(){
 	$con=dbCon();
-	$res=mysqli_query($con,"SELECT page FROM analytics WHERE created > (NOW() - INTERVAL $interval) GROUP BY page ORDER BY page ASC");
-	$val=array();
+	$res=mysqli_query($con,"SELECT page FROM analytics GROUP BY page ORDER BY page ASC");
 	while($dsatz=mysqli_fetch_assoc($res)){
 		$val[]=$dsatz['page'];
 	}
@@ -23,26 +22,38 @@ function getPages($interval="1 MONTH"){
 	return $val;
 }
 
-function getByCountry($interval="1 MONTH"){
+function echoData(){
 	$con=dbCon();
-	$val=array();
-	$sql="SELECT page,COUNT(*) as views FROM analytics WHERE created > (NOW() - INTERVAL $interval) GROUP BY page ORDER BY page ASC";
-	$res=mysqli_query($con,$sql);
-	mysqli_close($con);
-	/* TODO
+	$res=mysqli_query($con,"SELECT page,DATE(created) AS dateCreated,COUNT(*) AS views FROM analytics GROUP BY page,dateCreated ORDER BY page,dateCreated ASC");
+	echo 'Tag';
+	$pages=getPages();
+	foreach($pages as $page){
+		echo ",$page";
+	}
+	echo '\n';
+	$date="";
 	while($dsatz=mysqli_fetch_assoc($res)){
-		if(!isset($val[$dsatz['country']])){
-			$val[$dsatz['country']]=array($dsatz['views']);
-		}else{
-			$val[$dsatz['country']][]=$dsatz['views'];
+		if($dsatz['dateCreated']!=$date){
+			if(!empty($val)){
+				echo implode(',',$val).'\n';
+			}
+			foreach($pages as $page){
+				$val[$page]=0;
+			}
+			$date=$dsatz['dateCreated'];
+			echo date('j/n/y',strtotime($date)).',';
 		}
+		$val[$dsatz['page']]=$dsatz['views'];
 	}
-	foreach($val as $key => $value){
-		if(empty($key)){
-			$key="unknown";
-		}
-		$ret[]=array('name'=>$key,'data'=>$value);
+	echo implode(',',$val);
+	mysqli_close($con);
+}
+
+function echoSeries(){
+	$pages=getPages();
+	echo '[';
+	foreach($pages as $page){
+		echo "{name:\"$page\"},";
 	}
-	return $ret;
-	*/
+	echo ']';
 }
