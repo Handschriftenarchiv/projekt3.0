f815419224e9bd8b92ee2b6c053c2521b6cebd92
Handschriftenarchiv <marc.mitzscherling@protonmail.com>
2017-09-22 17:59:44 +0200

    Aufgehübscht
    …

--- not-found.php
+++ not-found.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+require_once 'analytics.php';
+?><!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
@@ -8,8 +10,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>404 - Handschriftenarchiv Dresdner Kreuzchor</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Offizielle Website des Handschriftenarchiv Dresdner Kreuzchor" />
-	<meta name="keywords" content="Handschrift, Kreuzchor, Geschichte, Archiv, Dresden" />
+	<meta name="description" content="Seite nicht gefunden" />
+	<meta name="keywords" content="" />
 	<meta name="author" content="Handschriftenarchiv Dresdner Kreuzchor" />
 
   <!--
@@ -26,89 +28,102 @@
 	//////////////////////////////////////////////////////
 	 -->
 
-  	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
+	 <!-- Facebook and Twitter integration -->
+ <meta property="og:title" content=""/>
+ <meta property="og:image" content=""/>
+ <meta property="og:url" content=""/>
+ <meta property="og:site_name" content=""/>
+ <meta property="og:description" content=""/>
+ <meta name="twitter:title" content="" />
+ <meta name="twitter:image" content="" />
+ <meta name="twitter:url" content="" />
+ <meta name="twitter:card" content="" />
 
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	<link rel="shortcut icon" href="favicon.ico">
+ <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+ <link rel="shortcut icon" href="favicon.ico">
 
-	<link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
 
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="css/bootstrap.css">
+ <!-- Animate.css -->
+ <link rel="stylesheet" href="css/animate.css">
+ <!-- Icomoon Icon Fonts-->
+ <link rel="stylesheet" href="css/icomoon.css">
+ <!-- Bootstrap  -->
+ <link rel="stylesheet" href="css/bootstrap.css">
 
-	<!-- Flexslider  -->
-	<link rel="stylesheet" href="css/flexslider.css">
+ <!-- Flexslider  -->
+ <link rel="stylesheet" href="css/flexslider.css">
 
-	<link rel="stylesheet" href="css/style.css">
+ <link rel="stylesheet" href="css/style.css">
 
 
-	<!-- Modernizr JS -->
-	<script src="js/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
+ <!-- Modernizr JS -->
+ <script src="js/modernizr-2.6.2.min.js"></script>
+ <!-- FOR IE9 below -->
+ <!--[if lt IE 9]>
+ <script src="js/respond.min.js"></script>
+ <![endif]-->
 
-	</head>
-	<body>
+ </head>
+ <body>
+	 <script type="text/javascript">
+window._idl = {};
+_idl.variant = "modal";
+(function() {
+	 var idl = document.createElement('script');
+	 idl.async = true;
+	 idl.src = 'https://members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'modal');
+	 document.getElementsByTagName('body')[0].appendChild(idl);
+})();
+</script>
 <!--
-		<nav id="fh5co-main-nav" role="navigation">
-			<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
-			<div class="js-fullheight fh5co-table">
-				<div class="fh5co-table-cell js-fullheight">
-					<h1 class="text-center"><a class="fh5co-logo" href="index.php">Sprache</a></h1>
-					<ul>
-						<li><a href="index.php">Deutsch</a></li>
-						<li><a href="/en/index.php">Englisch</a></li>
-					</ul>
-					<p class="fh5co-social-icon">
-						<a href="https://twitter.com/NotenarchivDKC"><i class="icon-twitter2"></i></a>
+	 <nav id="fh5co-main-nav" role="navigation">
+		 <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
+		 <div class="js-fullheight fh5co-table">
+			 <div class="fh5co-table-cell js-fullheight">
+				 <h1 class="text-center"><a class="fh5co-logo" href="index.php">Sprache</a></h1>
+				 <ul>
+					 <li><a href="index.php">Deutsch</a></li>
+					 <li><a href="/en/index.php">Englisch</a></li>
+				 </ul>
+				 <p class="fh5co-social-icon">
+					 <a href="https://twitter.com/NotenarchivDKC"><i class="icon-twitter2"></i></a>
 
-						<a href="https://issuu.com/hsa6"><i class="icon-book"></i></a>
-						<a href="https://vimeo.com/handschriftenarchiv"><i class="icon-vimeo"></i></a>
-						<a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"><i class="icon-youtube"></i></a>
-					</p>
-				</div>
-			</div>
-		</nav>
+					 <a href="https://issuu.com/hsa6"><i class="icon-book"></i></a>
+					 <a href="https://vimeo.com/handschriftenarchiv"><i class="icon-vimeo"></i></a>
+					 <a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"><i class="icon-youtube"></i></a>
+				 </p>
+			 </div>
+		 </div>
+	 </nav>
 -->
-	<nav id="fh5co-main-nav" role="navigation">
-		<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
-		<div class="js-fullheight fh5co-table">
-			<div class="fh5co-table-cell js-fullheight">
-				<h1 class="text-center"><a class="fh5co-logo" href="index.php">Handschriftenarchiv Dresdner Kreuzchor</a></h1>
-				<ul>
-					<li><a href="index.php">Willkommen</a></li>
-					<li><a href="archiv.php">Archiv</a></li>
-					<li><a href="/blog/">Blog</a></li>
-					<li><a href="mediathek.php">Mediathek</a></li>
-					<li><a href="/suche/">Datenbank</a></li>
-					<li><a href="impressum.php">Impressum</a></li>
-					<li><a href="kontakt.php">Kontakt</a></li>
-				</ul>
-				<p class="fh5co-social-icon">
-					<a href="https://twitter.com/NotenarchivDKC"><i class="icon-twitter2"></i></a>
-					<a href="https://issuu.com/hsa6"><i class="icon-book"></i></a>
-					<a href="https://vimeo.com/handschriftenarchiv"><i class="icon-vimeo"></i></a>
-					<a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"><i class="icon-youtube"></i></a>
-				</p>
-			</div>
-		</div>
-	</nav>
+ <nav id="fh5co-main-nav" role="navigation">
+	 <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
+	 <div class="js-fullheight fh5co-table">
+		 <div class="fh5co-table-cell js-fullheight">
+			 <h1 class="text-center"><a class="fh5co-logo" href="index.php">Handschriftenarchiv Dresdner Kreuzchor</a></h1>
+			 <ul>
+				 <li><a href="index.php">Willkommen</a></li>
+				 <li><a href="archiv.php">Archiv</a></li>
+				 <li><a href="/blog/">Blog</a></li>
+				 <li><a href="mediathek.php">Mediathek</a></li>
+				 <li><a href="/suche/">Datenbank</a></li>
+				 <li><a href="impressum.php">Impressum</a></li>
+				 <li><a href="kontakt.php">Kontakt</a></li>
+			 </ul>
+			 <p class="fh5co-social-icon">
+				 <a href="https://twitter.com/NotenarchivDKC"target="_blank"><i class="icon-twitter2"></i></a>
+				 <a href="https://issuu.com/hsa6"target="_blank"><i class="icon-book"></i></a>
+				 <a href="https://vimeo.com/handschriftenarchiv"target="_blank"><i class="icon-vimeo"></i></a>
+				 <a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"target="_blank"><i class="icon-youtube"></i></a>
+				 <a href="https://plus.google.com/108785494716898198379"target="_blank"><i class="icon-google"></i></a>
+				 <a href="https://github.com/handschriftenarchiv"target="_blank"><i class="icon-github2"></i></a>
+				 <a href="https://be.net/handschriftenarchiv"target="_blank"><i class="icon-behance"></i></a>
+			 </p>
+		 </div>
+	 </div>
+ </nav>
 
 	<div id="fh5co-page">
 		<header>
@@ -134,15 +149,15 @@
 		<aside id="fh5co-hero" class="js-fullheight">
 			<div class="flexslider js-fullheight">
 				<ul class="slides">
-			   	<li style="background-image: url(images/slide_1.jpg);">
+			   	<li style="background-image: url(images/404.svg);">
 			   		<div class="overlay-gradient"></div>
 			   		<div class="container">
 			   			<div class="col-md-8 col-md-offset-2 col-md-push-4 js-fullheight slider-text">
 			   				<div class="slider-text-inner">
 			   					<div class="desc">
-			   						<span>nicht gefunden</span>
-			   						<h2>404</h2>
-			   						<p>Es tut uns leid, aber wir konnten "<?php echo $_SERVER['REQUEST_URI']; ?>" leider nicht finden.</p>
+			   						<span>interner Fehler:</span>
+			   						<h2>Seite nicht gefunden</h2>
+			   						<p>Es tut uns leid, aber wir konnten "<?php echo $_SERVER['REQUEST_URI']; ?>" leider nicht finden. Versuchen Sie es mit Links aus dem Menü</p>
 			   					</div>
 			   				</div>
 			   			</div>
@@ -151,100 +166,81 @@
 			  	</ul>
 		  	</div>
 		</aside>
-		<div id="fh5co-intro-section">
-				<div id="fh5co-blog" class="fh5co-bg-section">
+	<footer>
+		<div id="footer">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-8 col-md-offset-2 animate-box text-center">
-						<h2 class="intro-heading">Handschriftenarchiv Dresdner Kreuzchor</h2>
-						<p><span>Noten, Drucke und Handschriften des Dresdner Kreuzchors</span></p>
-						<blockquote>
-							<p>Die ehemalige Handschriftenarchiv-Sammlung des Dresdner Kreuzchores bestand schon seit vielen Jahren, wurde jedoch erst 2013 erstmals offiziell als Archiv erwähnt. Hier werden die Noten aus 100 Jahren Dresdner Kreuzchor gesammelt, sortiert und archiviert. Das Archiv hat direkten Anschluss zur Notenbibliothek, sodass immer noch der Bezug zwischen alten und neuen Noten besteht. Im Archiv selbst finden sich Noten mit einem Alter bis zu 100 Jahren und Intoriten aus der Mauersberger-Zeit. Noten die vor 1945 geschrieben wurden sind Raritäten, denn größtenteils wurden die Notenbestände im Krieg vernichtet oder sind verschwunden. So sind nur noch einige wenige Exemplare erhalten geblieben. Seit zwei Jahren werden nun die Noten aus dem alten Sammlung in ein neues Archiv umsortiert und währenddessen gleich systematisch archiviert, erfasst und digitalisiert. Das ist neu, denn vor 2 Jahren noch wurden die Noten und Handschriften noch per Hand erfasst. Doch seit das Handschriftenarchiv auch im Netz vertreten ist, bietet es sich an nebenher Webpräsens die Handschriftlichen Noten in ein "neues" Zeitalter zu überführen - sie zu digitalisieren.</p>
-						</blockquote>
+					<div class="col-md-3">
+						<h3 class="section-title">Über das HSA</h3>
+						<p>Das Handschriftenarchiv Dresdner Kreuzchor sammelt, archiviert, digitalisiert und bereitet alte Drucke, Handschriften und Kopien des Dresdner Kreuzchores auf.</p>
+					</div>
+					<div class="col-md-3 col-md-push-1">
+						<h3 class="section-title">Links</h3>
+						<ul>
+							<li><a href="index.php">Willkommen</a></li>
+							<li><a href="archiv.php">Archiv</a></li>
+							<li><a href="/blog/">Blog</a></li>
+							<li><a href="mediathek.php">Mediathek</a></li>
+							<li><a href="/suche/">Datenbank</a></li>
+							<li><a href="impressum.php">Impressum</a></li>
+							<li><a href="kontakt.php">Kontakt</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<h3 class="section-title">Mitglied</h3>
+						<p>
+						<a href=https://www.deutsche-digitale-bibliothek.de/about-us/institutions/item/PXYIJUCJ654QX5KHG6E34VNE3UG3IXR2?query=Handschriftenarchiv+Dresdner+Kreuzchor target="_blank"><img src=images/ddb.png alt="Mitglied der Deutschen Digitalen Bibliothek" width=216 height=100 /></a>
+					</div>
+					<div class="col-md-3">
+						<h3 class="section-title">Handschriftenarchiv 3.0</h3>
+						<img src="images/wrapper-img.gif" width=231 height=130>
+						<!--<form class="form-inline" id="fh5co-header-subscribe">
+							<div class="row">
+								<div class="col-md-12 col-md-offset-0">
+									<div class="form-group">
+										<input type="text" class="form-control" id="email" placeholder="Enter your email">
+										<button type="submit" class="btn btn-default"><i class="icon-paper-plane"></i></button>
+									</div>
+								</div>
+							</div>
+						</form>-->
+					</div>
+				</div>
+				<div class="row copy-right">
+					<div class="col-md-6 col-md-offset-3 text-center">
+						<p class="fh5co-social-icon">
+							<a href="https://twitter.com/NotenarchivDKC"target="_blank"><i class="icon-twitter2"></i></a>
+							<a href="https://issuu.com/hsa6"target="_blank"><i class="icon-book"></i></a>
+							<a href="https://vimeo.com/handschriftenarchiv"target="_blank"><i class="icon-vimeo"></i></a>
+							<a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"target="_blank"><i class="icon-youtube"></i></a>
+							<a href="https://plus.google.com/108785494716898198379"target="_blank"><i class="icon-google"></i></a>
+							<a href="https://github.com/handschriftenarchiv"target="_blank"><i class="icon-github2"></i></a>
+							<a href="https://be.net/handschriftenarchiv"target="_blank"><i class="icon-behance"></i></a>
+						</p>
+						<p>Copyright 2016 Handschriftenarchiv Dresdner Kreuzchor <a href="/impressum.php">Impressum</a> All Rights Reserved. </p>
+						<span class="notranslate" onmouseover="_tipon(this)" onmouseout="_tipoff()"><span class="google-src-text" style="direction: ltr; text-align: left"><a href=https://translate.googleusercontent.com/translate_c?act=url&depth=1&hl=de&ie=UTF8&prev=_t&rurl=translate.google.de&sl=en&sp=nmt4&tl=de&u=https://internetdefenseleague.org/&usg=ALkJrhjDQg4VshLY48zmtw4d205QQxxxMg target="_blank"><img src=images/internet.png alt="Mitglied in der Internet Defense League" width=115 height=115/></a></span>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-		<footer>
-			<div id="footer" style="border:none;">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-3">
-							<h3 class="section-title">Über das HSA</h3>
-							<p>Das Handschriftenarchiv Dresdner Kreuzchor sammelt, archiviert, digitalisiert und bereitet alte Drucke, Handschriften und Kopien des Dresdner Kreuzchores auf.</p>
-						</div>
+	</footer>
 
-						<div class="col-md-3 col-md-push-1">
-							<h3 class="section-title">Links</h3>
-							<ul>
-								<li><a href="index.php">Willkommen</a></li>
-								<li><a href="archiv.php">Archiv</a></li>
-								<li><a href="/blog/">Blog</a></li>
-								<li><a href="mediathek.php">Mediathek</a></li>
-								<li><a href="/suche/">Datenbank</a></li>
-								<li><a href="impressum.php">Impressum</a></li>
-								<li><a href="kontakt.php">Kontakt</a></li>
-							</ul>
-						</div>
+</div>
 
-						<div class="col-md-3">
-							<h3 class="section-title">Recht</h3>
-							<ul>
-								<li><a href="/impressum.php">Impressum</a></li>
-								<li><a href="/impressum.php#recht">Rechtliche Informationen</a></li>
-								<li><a href="/impressum.php#daten">Datenschutz</a></li>
-								<li><a href="/archiv.php">Archivordnungen</a></li>
-							</ul>
-						</div>
-						<div class="col-md-3">
-							<h3 class="section-title">Handschriftenarchiv 3.0</h3>
-							<img src="images/wrapper-img.gif" width=231 height=130>
-							<!--<form class="form-inline" id="fh5co-header-subscribe">
-								<div class="row">
-									<div class="col-md-12 col-md-offset-0">
-										<div class="form-group">
-											<input type="text" class="form-control" id="email" placeholder="Enter your email">
-											<button type="submit" class="btn btn-default"><i class="icon-paper-plane"></i></button>
-										</div>
-									</div>
-								</div>
-							</form>-->
-						</div>
-					</div>
-					<div class="row copy-right">
-						<div class="col-md-6 col-md-offset-3 text-center">
-							<p class="fh5co-social-icon">
-								<a href="https://twitter.com/NotenarchivDKC"><i class="icon-twitter2"></i></a>
+<!-- jQuery -->
+<script src="js/jquery.min.js"></script>
+<!-- jQuery Easing -->
+<script src="js/jquery.easing.1.3.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Waypoints -->
+<script src="js/jquery.waypoints.min.js"></script>
+<!-- Counters -->
+<script src="js/jquery.countTo.js"></script>
+<!-- Flexslider -->
+<script src="js/jquery.flexslider-min.js"></script>
 
-								<a href="https://issuu.com/hsa6"><i class="icon-book"></i></a>
-								<a href="https://vimeo.com/handschriftenarchiv"><i class="icon-vimeo"></i></a>
-								<a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"><i class="icon-youtube"></i></a>
-							</p>
-							<p>Copyright 2016 Handschriftenarchiv Dresdner Kreuzchor <a href="/impressum.php">Impressum</a> All Rights Reserved. </p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-
-	</div>
-
-	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Counters -->
-	<script src="js/jquery.countTo.js"></script>
-	<!-- Flexslider -->
-	<script src="js/jquery.flexslider-min.js"></script>
-
-	<!-- Main JS (Do not remove) -->
-	<script src="js/main.js"></script>
-
-	</body>
+<!-- Main JS (Do not remove) -->
+<script src="js/main.js"></script>
+</body>
 </html>
