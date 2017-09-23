69473694dc21662f847eb69dfd6473a20446c9e7
Handschriftenarchiv <marc.mitzscherling@protonmail.com>
2017-09-23 16:45:09 +0200

    Report a Bug
    erste Ansätze für ein Formular

--- verwaltung/report.php
+++ verwaltung/report.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+require_once '../analytics.php';
+?><!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
@@ -6,10 +8,10 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Kontakt - Handschriftenarchiv Dresdner Kreuzchor</title>
+	<title>Fehlermeldung - Handschriftenarchiv Dresdner Kreuzchor</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Bleiben Sie in Kontakt mit dem Handschriftenarchiv" />
-	<meta name="keywords" content="Konatkt, Handschriftenarchiv, Archiv, Dresden, Kreuzchor, Handschriftenarchiv, Dresdner Kreuzchor" />
+	<meta name="description" content="Melden Sie einen Fehler" />
+	<meta name="keywords" content="Bug, Report, Handschriftenarchiv, Archiv, Dresden, Kreuzchor, Handschriftenarchiv, Dresdner Kreuzchor" />
 	<meta name="author" content="Handschriftenarchiv Dresdner Kreuzchor" />
 
   <!--
@@ -38,26 +40,26 @@
  <meta name="twitter:card" content="" />
 
  <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
- <link rel="shortcut icon" href="favicon.ico">
+ <link rel="shortcut icon" href="../favicon.ico">
 
  <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
  <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
 
  <!-- Animate.css -->
- <link rel="stylesheet" href="css/animate.css">
+ <link rel="stylesheet" href="../css/animate.css">
  <!-- Icomoon Icon Fonts-->
- <link rel="stylesheet" href="css/icomoon.css">
+ <link rel="stylesheet" href="../css/icomoon.css">
  <!-- Bootstrap  -->
- <link rel="stylesheet" href="css/bootstrap.css">
+ <link rel="stylesheet" href="../css/bootstrap.css">
 
  <!-- Flexslider  -->
- <link rel="stylesheet" href="css/flexslider.css">
+ <link rel="stylesheet" href="../css/flexslider.css">
 
- <link rel="stylesheet" href="css/style.css">
+ <link rel="stylesheet" href="../css/style.css">
 
 
  <!-- Modernizr JS -->
- <script src="js/modernizr-2.6.2.min.js"></script>
+ <script src="../js/modernizr-2.6.2.min.js"></script>
  <!-- FOR IE9 below -->
  <!--[if lt IE 9]>
  <script src="js/respond.min.js"></script>
@@ -100,15 +102,15 @@ _idl.variant = "modal";
 	 <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
 	 <div class="js-fullheight fh5co-table">
 		 <div class="fh5co-table-cell js-fullheight">
-			 <h1 class="text-center"><a class="fh5co-logo" href="index.php">Handschriftenarchiv Dresdner Kreuzchor</a></h1>
+			 <h1 class="text-center"><a class="fh5co-logo" href="../index.php">Handschriftenarchiv Dresdner Kreuzchor</a></h1>
 			 <ul>
-				 <li><a href="index.php">Willkommen</a></li>
-				 <li><a href="archiv.php">Archiv</a></li>
-				 <li><a href="/blog/">Blog</a></li>
-				 <li><a href="mediathek.php">Mediathek</a></li>
-				 <li><a href="/suche/">Datenbank</a></li>
-				 <li><a href="impressum.php">Impressum</a></li>
-				 <li><a href="kontakt.php">Kontakt</a></li>
+				 <li><a href="../index.php">Willkommen</a></li>
+				 <li><a href="../archiv.php">Archiv</a></li>
+				 <li><a href="..//blog/">Blog</a></li>
+				 <li><a href="../mediathek.php">Mediathek</a></li>
+				 <li><a href="../suche/">Datenbank</a></li>
+				 <li><a href="../impressum.php">Impressum</a></li>
+				 <li><a href="../kontakt.php">Kontakt</a></li>
 			 </ul>
 			 <p class="fh5co-social-icon">
 				 <a href="https://twitter.com/NotenarchivDKC"target="_blank"><i class="icon-twitter2"></i></a>
@@ -129,7 +131,7 @@ _idl.variant = "modal";
 			 <div class="fh5co-navbar-brand">
 				 <div class="row">
 					 <div class="col-xs-6">
-						 <h1 class="text-left"><a class="fh5co-logo" href="index.php"><span>Archiv</span> Handschriftenarchiv <span>Dresdner Kreuzchor</span></a></h1>
+						 <h1 class="text-left"><a class="fh5co-logo" href="../index.php"><span>Archiv</span> Handschriftenarchiv <span>Dresdner Kreuzchor</span></a></h1>
 					 </div>
 					 <div class="col-xs-6">
 						 <!--<p class="fh5co-social-icon text-right">
@@ -150,15 +152,15 @@ _idl.variant = "modal";
 		<aside id="fh5co-hero" class="js-fullheight">
 			<div class="flexslider js-fullheight">
 				<ul class="slides">
-			   	<li style="background-image: url(images/kontakt/start.svg);">
+			   	<li style="background-image: url(../images/kontakt/start.svg);">
 			   		<div class="overlay-gradient"></div>
 			   		<div class="container">
 			   			<div class="col-md-8 col-md-offset-2 col-md-push-4 js-fullheight slider-text">
 			   				<div class="slider-text-inner">
 			   					<div class="desc">
-			   						<span>Kontaktinformationen</span>
-			   						<h2>Konatkt</h2>
-			   						<p class="fh5co-lead">Bleiben Sie mit uns in Verbindung. Schreiben Sie uns.</p>
+			   						<span>Report a Bug</span>
+			   						<h2>Fehler melden</h2>
+			   						<p class="fh5co-lead">Helfen Sie uns bei der Verbesserung unseres Services.</p>
 			   					</div>
 			   				</div>
 			   			</div>
@@ -172,28 +174,27 @@ _idl.variant = "modal";
 			<div class="container">
 				<div class="row">
 					<div class="col-md-3 col-md-push-1 animate-box">
-						<h3>Kontaktangaben</h3>
+						<h3>Fehler-Kategorien</h3>
 						<ul class="contact-info">
-							<li><i class="icon-location-pin"></i>Dornblüthstraße 4, 01277 Dresden, Deutschland</li>
-							<li><i class="icon-phone2"></i><span>0351</span><span>3153572</span></li>
-							<li><i class="icon-mail"></i>handschriftenarchiv<br>[ät]protonmail.com</li>
-							<li><i class="icon-globe2"></i><a href="http://notenarchiv-dkc.bplaced.com/">hsa.bplaced.com</a></li>
+							<li><i class="icon-database"></i>Datenbankeintrag</li>
+							<li><i class="icon-pencil2"></i>Rechtschreibung</li>
+							<li><i class="icon-cog"></i>Fehlerhafte Funktion</li>
+							<li><i class="icon-circle"></i>Sonstige Fehler</li>
 						</ul>
 					</div>
 					<div class="col-md-7 col-md-push-1 animate-box">
 						<?php
-						$process=isset($_POST['name']);
-						$valid=!empty($_POST['email'])&&filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+						$valid=!empty($_POST['location'])&&filter_var($_POST['location']);
 						if($process&&$valid){
 							$header="From: \"".str_replace('"',"",$_POST[name])."\" <$_POST[email]>\nContent-Type: text/plain\n";
-							if(mail('handschriftenarchiv@protonmail.com','Anfrage Kontakt - Handschriftenarchiv Dresdner Kreuzchor',$_POST['text'],$header)){
-								echo "<p>Ihre Nachricht wurde erfolgreich an uns versandt. Wir werden Ihnen so schnell wie möglich antworten.</p>";
+							if(mail('handschriftenarchiv@protonmail.com','Fehlermeldung Kontakt - Handschriftenarchiv Dresdner Kreuzchor',$_POST['text'],$header)){
+								echo "<p>Der Fehler wurde gemeldet.</p>";
 								$mail=true;
 							}else{
-								echo "<p>Leider konnte Ihre Nachricht nicht versendet werden.</p>";
+								echo "<p>Der Fehöer konnte leider nicht gesendet werden.</p>";
 							}
 						}elseif($process){
-							echo "<p>Bitte geben Sie eine gültige E-Mail-Adresse ein!</p>";
+							echo "<p>Bitte geben Sie einen gültigen Ort ein!</p>";
 						}
 						if(!isset($mail)){
 						?>
@@ -201,27 +202,27 @@ _idl.variant = "modal";
 							<div class="row">
 								<div class="col-md-6">
 									<div class="form-group">
-										<input type="text" class="form-control" placeholder="Name" name="name"<?php if(isset($_POST['name'])){echo " value=\"$_POST[name]\"";}?>>
+										<input type="text" class="form-control" placeholder="Fehlerkategorie" name="category" <?php if(isset($_POST['name'])){echo " value=\"$_POST[name]\"";}?>>
 									</div>
 								</div>
 								<div class="col-md-6">
 									<div class="form-group">
-										<input type="text" class="form-control" placeholder="E-Mail" name="email"<?php if(isset($_POST['email'])){echo " value=\"$_POST[email]\"";}?>>
+										<input type="text" class="form-control" placeholder="Wo?" name="location" <?php if(isset($_POST['location'])){echo " value=\"$_POST[location]\"";}?>>
 									</div>
 								</div>
 								<div class="col-md-12">
 									<div class="form-group">
-										<textarea name="text" class="form-control" id="" cols="30" rows="7" placeholder="Nachricht"><?php if(isset($_POST['text'])){echo $_POST['text'];}?></textarea>
+										<textarea name="text" class="form-control" id="" cols="30" rows="7" placeholder="Fehler-Beschreibung" <?php if(isset($_POST['name'])){echo " value=\"$_POST[name]\"";}?>></textarea>
 									</div>
 								</div>
 								<div class="col-md-12">
 									<div class="form-group">
-										<input type="submit" value="Nachricht senden" class="btn btn-primary">
+										<input type="submit" value="Fehler melden" class="btn btn-primary">
 									</div>
 								</div>
 							</div>
 						</form>
-						<?php } ?>
+					<?php } ?>
 					</div>
 				</div>
 			</div>
@@ -240,24 +241,24 @@ _idl.variant = "modal";
 						<div class="col-md-3 col-md-push-1">
 							<h3 class="section-title">Links</h3>
 							<ul>
-								<li><a href="index.php">Willkommen</a></li>
-								<li><a href="archiv.php">Archiv</a></li>
-								<li><a href="/blog/">Blog</a></li>
-								<li><a href="mediathek.php">Mediathek</a></li>
-								<li><a href="/suche/">Datenbank</a></li>
-								<li><a href="impressum.php">Impressum</a></li>
-								<li><a href="kontakt.php">Kontakt</a></li>
+								<li><a href="../index.php">Willkommen</a></li>
+								<li><a href="../archiv.php">Archiv</a></li>
+								<li><a href="../blog/">Blog</a></li>
+								<li><a href="../mediathek.php">Mediathek</a></li>
+								<li><a href="../suche/">Datenbank</a></li>
+								<li><a href="../impressum.php">Impressum</a></li>
+								<li><a href="../kontakt.php">Kontakt</a></li>
 							</ul>
 						</div>
 
 						<div class="col-md-3">
 							<h3 class="section-title">Mitglied</h3>
 							<p>
-							<a href=https://www.deutsche-digitale-bibliothek.de/about-us/institutions/item/PXYIJUCJ654QX5KHG6E34VNE3UG3IXR2?query=Handschriftenarchiv+Dresdner+Kreuzchor target="_blank"><img src=images/ddb.png alt="Mitglied der Deutschen Digitalen Bibliothek" width=216 height=100 /></a>
+							<a href=https://www.deutsche-digitale-bibliothek.de/about-us/institutions/item/PXYIJUCJ654QX5KHG6E34VNE3UG3IXR2?query=Handschriftenarchiv+Dresdner+Kreuzchor target="_blank"><img src=../images/ddb.png alt="Mitglied der Deutschen Digitalen Bibliothek" width=216 height=100 /></a>
 						</div>
 						<div class="col-md-3">
 							<h3 class="section-title">Handschriftenarchiv 3.0</h3>
-							<img src="images/wrapper-img.gif" width=231 height=130>
+							<img src="../images/wrapper-img.gif" width=231 height=130>
 							<!--<form class="form-inline" id="fh5co-header-subscribe">
 								<div class="row">
 									<div class="col-md-12 col-md-offset-0">
@@ -281,8 +282,8 @@ _idl.variant = "modal";
 								<a href="https://github.com/handschriftenarchiv"target="_blank"><i class="icon-github2"></i></a>
 								<a href="https://be.net/handschriftenarchiv"target="_blank"><i class="icon-behance"></i></a>
 							</p>
-							<p>Copyright 2016 Handschriftenarchiv Dresdner Kreuzchor <a href="/impressum.php">Impressum</a> All Rights Reserved. </p>
-							<span class="notranslate" onmouseover="_tipon(this)" onmouseout="_tipoff()"><span class="google-src-text" style="direction: ltr; text-align: left"><a href=https://translate.googleusercontent.com/translate_c?act=url&depth=1&hl=de&ie=UTF8&prev=_t&rurl=translate.google.de&sl=en&sp=nmt4&tl=de&u=https://internetdefenseleague.org/&usg=ALkJrhjDQg4VshLY48zmtw4d205QQxxxMg target="_blank"><img src=images/internet.png alt="Mitglied in der Internet Defense League" width=115 height=115/></a></span>
+							<p>Copyright 2016 Handschriftenarchiv Dresdner Kreuzchor <a href="../impressum.php">Impressum</a> All Rights Reserved. </p>
+							<span class="notranslate" onmouseover="_tipon(this)" onmouseout="_tipoff()"><span class="google-src-text" style="direction: ltr; text-align: left"><a href=https://translate.googleusercontent.com/translate_c?act=url&depth=1&hl=de&ie=UTF8&prev=_t&rurl=translate.google.de&sl=en&sp=nmt4&tl=de&u=https://internetdefenseleague.org/&usg=ALkJrhjDQg4VshLY48zmtw4d205QQxxxMg target="_blank"><img src=../images/internet.png alt="Mitglied in der Internet Defense League" width=115 height=115/></a></span>
 						</div>
 					</div>
 				</div>
@@ -292,19 +293,19 @@ _idl.variant = "modal";
 	</div>
 
 	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
+	<script src="../js/jquery.min.js"></script>
 	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
+	<script src="../js/jquery.easing.1.3.js"></script>
 	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
 	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="../js/jquery.waypoints.min.js"></script>
 	<!-- Counters -->
-	<script src="js/jquery.countTo.js"></script>
+	<script src="../js/jquery.countTo.js"></script>
 	<!-- Flexslider -->
-	<script src="js/jquery.flexslider-min.js"></script>
+	<script src="../js/jquery.flexslider-min.js"></script>
 
 	<!-- Main JS (Do not remove) -->
-	<script src="js/main.js"></script>
+	<script src="../js/main.js"></script>
 	</body>
 </html>
