d2b9414f673bcc8e8d89e7e36e843ae0e513d652
Handschriftenarchiv <marc.mitzscherling@protonmail.com>
2017-10-19 20:19:25 +0200

    Google Translator Testfeature
    um Übersetzungen vorzeitig anbieten zu können im Sinne von #4

--- index.beta.php
+++ index.beta.php
@@ -1,6 +1,12 @@
 <?php
 require_once "analytics.php";
-?><!DOCTYPE html>
+?>
+<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'de', includedLanguages: 'en,es,fi,fr,it,ja,ru,zh-CN', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
