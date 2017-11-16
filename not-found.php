d32350ef4ae82c3b95306f656b03da1aa7243170
Johann150 <Johann150@users.noreply.github.com>
2017-11-16 12:27:42 +0100

    Einrückung korrigiert

--- not-found.php
+++ not-found.php
@@ -15,7 +15,7 @@ require_once 'translate.php';
 	<meta name="keywords" content="" />
 	<meta name="author" content="Handschriftenarchiv Dresdner Kreuzchor" />
 
-  <!--
+	<!--
 	//////////////////////////////////////////////////////
 
 	FREE HTML5 TEMPLATE
@@ -27,154 +27,154 @@ require_once 'translate.php';
 	Facebook: 		https://www.facebook.com/fh5co
 
 	//////////////////////////////////////////////////////
-	 -->
+	-->
 
-	 <!-- Facebook and Twitter integration -->
- <meta property="og:title" content=""/>
- <meta property="og:image" content=""/>
- <meta property="og:url" content=""/>
- <meta property="og:site_name" content=""/>
- <meta property="og:description" content=""/>
- <meta name="twitter:title" content="" />
- <meta name="twitter:image" content="" />
- <meta name="twitter:url" content="" />
- <meta name="twitter:card" content="" />
+	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
 
- <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
- <link rel="shortcut icon" href="/favicon.ico">
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="/favicon.ico">
 
- <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
- <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
 
- <!-- Animate.css -->
- <link rel="stylesheet" href="/css/animate.css">
- <!-- Icomoon Icon Fonts-->
- <link rel="stylesheet" href="/css/icomoon.css">
- <!-- Bootstrap  -->
- <link rel="stylesheet" href="/css/bootstrap.css">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="/css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="/css/bootstrap.css">
 
- <!-- Flexslider  -->
- <link rel="stylesheet" href="/css/flexslider.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="/css/flexslider.css">
 
- <link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="/css/style.css">
 
 
- <!-- Modernizr JS -->
- <script src="/js/modernizr-2.6.2.min.js"></script>
- <!-- FOR IE9 below -->
- <!--[if lt IE 9]>
- <script src="js/respond.min.js"></script>
- <![endif]-->
+	<!-- Modernizr JS -->
+	<script src="/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
 
- </head>
- <body>
-	 <script type="text/javascript">
-window._idl = {};
-_idl.variant = "modal";
-(function() {
-	 var idl = document.createElement('script');
-	 idl.async = true;
-	 idl.src = 'https://members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'modal');
-	 document.getElementsByTagName('body')[0].appendChild(idl);
-})();
-</script>
+	</head>
+	<body>
+		<script type="text/javascript">
+			window._idl = {};
+			_idl.variant = "modal";
+			(function() {
+					var idl = document.createElement('script');
+					idl.async = true;
+					idl.src = 'https://members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'modal');
+					document.getElementsByTagName('body')[0].appendChild(idl);
+			})();
+		</script>
 <!--
-	 <nav id="fh5co-main-nav" role="navigation">
-		 <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
-		 <div class="js-fullheight fh5co-table">
-			 <div class="fh5co-table-cell js-fullheight">
-				 <h1 class="text-center"><a class="fh5co-logo" href="index.php">Sprache</a></h1>
-				 <ul>
-					 <li><a href="index.php">Deutsch</a></li>
-					 <li><a href="/en/index.php">Englisch</a></li>
-				 </ul>
-				 <p class="fh5co-social-icon">
-					 <a href="https://twitter.com/NotenarchivDKC"><i class="icon-twitter2"></i></a>
+		<nav id="fh5co-main-nav" role="navigation">
+			<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
+			<div class="js-fullheight fh5co-table">
+				<div class="fh5co-table-cell js-fullheight">
+					<h1 class="text-center"><a class="fh5co-logo" href="index.php">Sprache</a></h1>
+					<ul>
+						<li><a href="index.php">Deutsch</a></li>
+						<li><a href="/en/index.php">Englisch</a></li>
+					</ul>
+					<p class="fh5co-social-icon">
+						<a href="https://twitter.com/NotenarchivDKC"><i class="icon-twitter2"></i></a>
 
-					 <a href="https://issuu.com/hsa6"><i class="icon-book"></i></a>
-					 <a href="https://vimeo.com/handschriftenarchiv"><i class="icon-vimeo"></i></a>
-					 <a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"><i class="icon-youtube"></i></a>
-				 </p>
-			 </div>
-		 </div>
-	 </nav>
--->
- <nav id="fh5co-main-nav" role="navigation">
-	 <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
-	 <div class="js-fullheight fh5co-table">
-		 <div class="fh5co-table-cell js-fullheight">
-			 <h1 class="text-center"><a class="fh5co-logo" href="index.php">Handschriftenarchiv Dresdner Kreuzchor</a></h1>
-			 <ul>
-				 <li><a href=".">Willkommen</a></li>
-				 <li><a href="/archiv">Archiv</a></li>
-				 <li><a href="/blog/">Blog</a></li>
-				 <li><a href="/mediathek">Mediathek</a></li>
-				 <li><a href="/suche/">Datenbank</a></li>
-				 <li><a href="/impressum">Impressum</a></li>
-				 <li><a href="/kontakt">Kontakt</a></li>
-			 </ul>
-			 <p class="fh5co-social-icon">
-				 <a href="https://twitter.com/NotenarchivDKC"target="_blank"><i class="icon-twitter2"></i></a>
-				 <a href="https://issuu.com/hsa6"target="_blank"><i class="icon-book"></i></a>
-				 <a href="https://vimeo.com/handschriftenarchiv"target="_blank"><i class="icon-vimeo"></i></a>
-				 <a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"target="_blank"><i class="icon-youtube"></i></a>
-				 <a href="https://plus.google.com/108785494716898198379"target="_blank"><i class="icon-google"></i></a>
-				 <a href="https://github.com/handschriftenarchiv"target="_blank"><i class="icon-github2"></i></a>
-				 <a href="https://be.net/handschriftenarchiv"target="_blank"><i class="icon-behance"></i></a>
-			 </p>
-		 </div>
-	 </div>
- </nav>
-
-	<div id="fh5co-page">
-		<header>
-			<div class="container">
-				<div class="fh5co-navbar-brand">
-					<div class="row">
-						<div class="col-xs-6">
-							<h1 class="text-left"><a class="fh5co-logo" href="index.php"><span>Archiv</span> Handschriftenarchiv <span>Dresdner Kreuzchor</span></a></h1>
-						</div>
-					</div>
-					<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+						<a href="https://issuu.com/hsa6"><i class="icon-book"></i></a>
+						<a href="https://vimeo.com/handschriftenarchiv"><i class="icon-vimeo"></i></a>
+						<a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"><i class="icon-youtube"></i></a>
+					</p>
 				</div>
 			</div>
-		</header>
-		<aside id="fh5co-hero" class="js-fullheight">
-			<div class="flexslider js-fullheight">
-				<ul class="slides">
-			   	<li style="background-image: url(/images/404.svg);">
-			   		<div class="overlay-gradient"></div>
-			   		<div class="container">
-			   			<div class="col-md-8 col-md-offset-2 col-md-push-4 js-fullheight slider-text">
-			   				<div class="slider-text-inner">
-			   					<div class="desc">
-			   						<?php echo __page('404');?>
-			   					</div>
-			   				</div>
-			   			</div>
-			   		</div>
-			   	</li>
-			  	</ul>
-		  	</div>
-		</aside>
-	<?php echo __page('footer');?>
+		</nav>
+-->
+		<nav id="fh5co-main-nav" role="navigation">
+			<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
+			<div class="js-fullheight fh5co-table">
+				<div class="fh5co-table-cell js-fullheight">
+					<h1 class="text-center"><a class="fh5co-logo" href="index.php">Handschriftenarchiv Dresdner Kreuzchor</a></h1>
+					<ul>
+						<li><a href=".">Willkommen</a></li>
+						<li><a href="/archiv">Archiv</a></li>
+						<li><a href="/blog/">Blog</a></li>
+						<li><a href="/mediathek">Mediathek</a></li>
+						<li><a href="/suche/">Datenbank</a></li>
+						<li><a href="/impressum">Impressum</a></li>
+						<li><a href="/kontakt">Kontakt</a></li>
+					</ul>
+					<p class="fh5co-social-icon">
+						<a href="https://twitter.com/NotenarchivDKC"target="_blank"><i class="icon-twitter2"></i></a>
+						<a href="https://issuu.com/hsa6"target="_blank"><i class="icon-book"></i></a>
+						<a href="https://vimeo.com/handschriftenarchiv"target="_blank"><i class="icon-vimeo"></i></a>
+						<a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"target="_blank"><i class="icon-youtube"></i></a>
+						<a href="https://plus.google.com/108785494716898198379"target="_blank"><i class="icon-google"></i></a>
+						<a href="https://github.com/handschriftenarchiv"target="_blank"><i class="icon-github2"></i></a>
+						<a href="https://be.net/handschriftenarchiv"target="_blank"><i class="icon-behance"></i></a>
+					</p>
+				</div>
+			</div>
+		</nav>
 
-</div>
+		<div id="fh5co-page">
+			<header>
+				<div class="container">
+					<div class="fh5co-navbar-brand">
+						<div class="row">
+							<div class="col-xs-6">
+								<h1 class="text-left"><a class="fh5co-logo" href="index.php"><span>Archiv</span> Handschriftenarchiv <span>Dresdner Kreuzchor</span></a></h1>
+							</div>
+						</div>
+						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+					</div>
+				</div>
+			</header>
+			<aside id="fh5co-hero" class="js-fullheight">
+				<div class="flexslider js-fullheight">
+					<ul class="slides">
+						<li style="background-image: url(/images/404.svg);">
+							<div class="overlay-gradient">
+							</div>
+							<div class="container">
+								<div class="col-md-8 col-md-offset-2 col-md-push-4 js-fullheight slider-text">
+									<div class="slider-text-inner">
+										<div class="desc">
+											<?php echo __page('404');?>
+										</div>
+									</div>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</aside>
+			<?php echo __page('footer');?>
+		</div>
 
-<!-- jQuery -->
-<script src="/js/jquery.min.js"></script>
-<!-- jQuery Easing -->
-<script src="/js/jquery.easing.1.3.js"></script>
-<!-- Bootstrap -->
-<script src="/js/bootstrap.min.js"></script>
-<!-- Waypoints -->
-<script src="/js/jquery.waypoints.min.js"></script>
-<!-- Counters -->
-<script src="/js/jquery.countTo.js"></script>
-<!-- Flexslider -->
-<script src="/js/jquery.flexslider-min.js"></script>
+		<!-- jQuery -->
+		<script src="/js/jquery.min.js"></script>
+		<!-- jQuery Easing -->
+		<script src="/js/jquery.easing.1.3.js"></script>
+		<!-- Bootstrap -->
+		<script src="/js/bootstrap.min.js"></script>
+		<!-- Waypoints -->
+		<script src="/js/jquery.waypoints.min.js"></script>
+		<!-- Counters -->
+		<script src="/js/jquery.countTo.js"></script>
+		<!-- Flexslider -->
+		<script src="/js/jquery.flexslider-min.js"></script>
 
-<!-- Main JS (Do not remove) -->
-<script src="/js/main.js"></script>
-</body>
+		<!-- Main JS (Do not remove) -->
+		<script src="/js/main.js"></script>
+	</body>
 </html>
