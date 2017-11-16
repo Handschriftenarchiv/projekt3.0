40d39363e5888c76c5c299238a39b2385de311e3
Johann150 <Johann150@users.noreply.github.com>
2017-11-16 12:37:27 +0100

    Navigation übersetzt

--- impressum.php
+++ impressum.php
@@ -99,32 +99,7 @@ require_once "translate.php";
 			</div>
 		</nav>
 -->
-		<nav id="fh5co-main-nav" role="navigation">
-			<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
-			<div class="js-fullheight fh5co-table">
-				<div class="fh5co-table-cell js-fullheight">
-					<h1 class="text-center"><a class="fh5co-logo" href="index.php"><?php echo __('hsa');?></a></h1>
-					<ul>
-						<li><a href="."><?php echo __('welcome');?></a></li>
-						<li><a href="/archiv"><?php echo __('archive');?></a></li>
-						<li><a href="/blog/"><?php echo __('blog');?></a></li>
-						<li><a href="/mediathek"><?php echo __('medialib');?></a></li>
-						<li><a href="/suche/"><?php echo __('db');?></a></li>
-						<li><a href="/impressum"><?php echo __('sitenotice');?></a></li>
-						<li><a href="/kontakt"><?php echo __('contact');?></a></li>
-					</ul>
-					<p class="fh5co-social-icon">
-						<a href="https://twitter.com/NotenarchivDKC"target="_blank"><i class="icon-twitter2"></i></a>
-						<a href="https://issuu.com/hsa6"target="_blank"><i class="icon-book"></i></a>
-						<a href="https://vimeo.com/handschriftenarchiv"target="_blank"><i class="icon-vimeo"></i></a>
-						<a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"target="_blank"><i class="icon-youtube"></i></a>
-						<a href="https://plus.google.com/108785494716898198379"target="_blank"><i class="icon-google"></i></a>
-						<a href="https://github.com/handschriftenarchiv"target="_blank"><i class="icon-github2"></i></a>
-						<a href="https://be.net/handschriftenarchiv"target="_blank"><i class="icon-behance"></i></a>
-					</p>
-				</div>
-			</div>
-		</nav>
+		<?php echo __page('nav');?>
 
 		<div id="fh5co-page">
 			<header>
