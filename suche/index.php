da0c35bb485363822bcda746a76b8c3936efa1dc
Unknown <johann.galle@gmx.net>
2017-11-17 18:09:36 +0100

    Datenbank-Suche übersetzt

--- suche/index.php
+++ suche/index.php
@@ -19,7 +19,7 @@ require_once '../translate.php';
 	<body onload="start()">
 		<a href=".."><img border="0" src="../favicon.ico" id="icon"/></a>
 		<div id="search">
-			<span style="font-size:16pt;">Volltext-Suche</span>
+			<span style="font-size:16pt;"><?php echo __('fulltextsrc');?></span>
 			<form method="GET" action="search.php" autocomplete="off" onsubmit="return checkSubmit(0);">
 				<input type="search" list="suggestions" name="search" id="searchBox" style="width:80%" oninput="lookup();"/>
 				<div id="livesearch"></div><br>
@@ -27,19 +27,19 @@ require_once '../translate.php';
 			</form>
 		</div>
 		<div id="search-b">
-			<span style="font-size:16pt;">erweiterte Suche</span>
+			<span style="font-size:16pt;"><?php echo __('advsrc');?></span>
 			<form method="GET" action="search.php" autocomplete="off" onsubmit="return checkSubmit(1);">
-				Verknüpfung der Suchargumente:
+				<?php echo __('logicalcon');?>:
 				<select name="mode" onchange="checkModeSwitch()">
-					<option selected value="AND">UND</option>
-					<option value="OR">ODER</option>
+					<option selected value="AND"><?php echo __('and');?></option>
+					<option value="OR"><?php echo __('or');?></option>
 				</select>
 				<table>
 					<tbody id="searchelems"><tr><td><select name="search[]" class="srch" onfocus="this.oldvalue=this.value;" onchange="checkSelect(this);this.oldvalue=this.value;">
-						<option>ID</option><option>Titel</option><option>Komponist</option><option>Bearbeiter</option><option>Dichter</option><option>Setzer</option>
-						<option>Typus</option><option>Verlag</option><option>Sprache</option><option>Schrift</option><option>Anzahl</option><option>Sammlung</option>
-						<option>Standort</option><option>Signatur</option><option value="Audiolink">Hörbeispiel verfügbar</option>
-						<option value="Dokumentlink">Digitalisierungen verfügbar</option></select></td><td class="text"><input name="val[]" type="number"></td><td></td></tr></tbody>
+						<option><?php echo __('id');?></option><option><?php echo __('title');?></option><option><?php echo __('comp');?></option><option><?php echo __('editor');?></option><option><?php echo __('poet');?></option><option><?php echo __('setter');?></option>
+						<option><?php echo __('typ');?></option><option><?php echo __('pub');?></option><option><?php echo __('lang');?></option><option><?php echo __('script');?></option><option><?php echo __('num');?></option><option><?php echo __('collection');?></option>
+						<option><?php echo __('loc');?></option><option><?php echo __('sig');?></option><option value="Audiolink"><?php echo __('audiolink+');?></option>
+						<option value="Dokumentlink"><?php echo __('digi+');?></option></select></td><td class="text"><input name="val[]" type="number"></td><td></td></tr></tbody>
 					<tfoot><tr><td><button type="button" onclick="nfield()" id="add"/></td><td colspan="2"></td></tr></tfoot>
 				</table>
 				<button type="submit" class="go"/>
