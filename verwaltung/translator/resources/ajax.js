ed83f86b3749eced028545eb6409b269333ca80a
Johann150 <johann.galle@protonmail.com>
2017-11-30 13:40:19 +0100

    Fehler behoben

--- verwaltung/translator/resources/ajax.js
+++ verwaltung/translator/resources/ajax.js
@@ -15,6 +15,21 @@ function getJSON(url,callback){
 }
 
 /**
+@param url The URL the data should be fetched from
+@param callback function that is called on success. only parameter can be the response text.
+*/
+function getText(url,callback){
+	var req = new XMLHttpRequest();
+	req.open('GET', url, true);
+	req.onload = () => {
+		if(req.status>=200&&req.status<400){
+			callback(req.responseText);
+		}
+	};
+	req.send();
+}
+
+/**
 @param url The URL the data should be sent to
 @param data The data that should be sent. (a JSON object)
 @param callback (optional) function that is called on success. only parameter can be the text that was the response
