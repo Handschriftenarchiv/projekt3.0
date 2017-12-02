606a200bb0de294114a2eda18b821646490f5361
Johann150 <johann.galle@protonmail.com>
2017-12-02 10:15:20 +0100

    require vergessen

--- blog/index.php
+++ blog/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../analytics.php';
+require_once '../translate.php';
 ?><!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
