3b77ea5537971fbaf926c15d040cd324f4f9e986
Johann150 <johann.galle@protonmail.com>
2017-12-03 21:38:33 +0100

    Links im Menü mit Sprache
    bei den Menü-Links wird jetzt die in der URL angegebene Sprache beibehalten

---
+++ lang/nav.php
@@ -0,0 +1,27 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/translate.php'; ?>
+<nav id="fh5co-main-nav" role="navigation">
+	<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
+	<div class="js-fullheight fh5co-table">
+		<div class="fh5co-table-cell js-fullheight">
+			<h1 class="text-center"><a class="fh5co-logo" href="/<?php echo $use_lang; ?>"><?php echo __('hsa'); ?></a></h1>
+			<ul>
+				<li><a href="/<?php echo $use_lang; ?>"><?php echo __('welcome'); ?></a></li>
+				<li><a href="/<?php echo $use_lang; ?>/archiv"><?php echo __('archive'); ?></a></li>
+				<li><a href="/<?php echo $use_lang; ?>/blog"><?php echo __('blog'); ?></a></li>
+				<li><a href="/<?php echo $use_lang; ?>/mediathek"><?php echo __('medialib'); ?></a></li>
+				<li><a href="/<?php echo $use_lang; ?>/suche"><?php echo __('db'); ?></a></li>
+				<li><a href="/<?php echo $use_lang; ?>/impressum"><?php echo __('sitenotice'); ?></a></li>
+				<li><a href="/<?php echo $use_lang; ?>/kontakt"><?php echo __('contact'); ?></a></li>
+			</ul>
+			<p class="fh5co-social-icon">
+				<a href="https://twitter.com/NotenarchivDKC" target="_blank"><i class="icon-twitter2"></i></a>
+				<a href="https://issuu.com/hsa6" target="_blank"><i class="icon-book"></i></a>
+				<a href="https://vimeo.com/handschriftenarchiv" target="_blank"><i class="icon-vimeo"></i></a>
+				<a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw" target="_blank"><i class="icon-youtube"></i></a>
+				<a href="https://plus.google.com/108785494716898198379" target="_blank"><i class="icon-google"></i></a>
+				<a href="https://github.com/handschriftenarchiv" target="_blank"><i class="icon-github2"></i></a>
+				<a href="https://be.net/handschriftenarchiv" target="_blank"><i class="icon-behance"></i></a>
+			</p>
+		</div>
+	</div>
+</nav>
