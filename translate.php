a573b578fe476c42072448c56520c4ef2ab53548
Johann150 <johann.galle@protonmail.com>
2017-12-12 21:13:35 +0100

    falscher Pfad

--- translate.php
+++ translate.php
@@ -4,8 +4,7 @@ the directory in which the dictionaries reside in
 
 standard is 'lang'
 */
-// $dict_dir=$_SERVER['DOCUMENT_ROOT'].'/lang';
-$dict_dir='C:\xampp\htdocs\hsa\lang';
+$dict_dir=$_SERVER['DOCUMENT_ROOT'].'/lang';
 /*
 language to use by default
 NULL or unset means to use the language provided by the browser
