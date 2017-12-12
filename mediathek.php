d6dcf0982331369809388911aba2c7b2e527ed44
Johann150 <johann.galle@protonmail.com>
2017-12-12 21:44:15 +0100

    Pfade berichtigt

--- mediathek.php
+++ mediathek.php
@@ -47,23 +47,23 @@ require_once 'translate.php';
 		<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
 
 		<!-- Animate.css -->
-		<link rel="stylesheet" href="css/animate.css">
+		<link rel="stylesheet" href="/css/animate.css">
 		<!-- Icomoon Icon Fonts-->
-		<link rel="stylesheet" href="css/icomoon.css">
+		<link rel="stylesheet" href="/css/icomoon.css">
 		<!-- Bootstrap  -->
-		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="/css/bootstrap.css">
 
 		<!-- Flexslider  -->
-		<link rel="stylesheet" href="css/flexslider.css">
+		<link rel="stylesheet" href="/css/flexslider.css">
 
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="/css/style.css">
 
 
 		<!-- Modernizr JS -->
-		<script src="js/modernizr-2.6.2.min.js"></script>
+		<script src="/js/modernizr-2.6.2.min.js"></script>
 		<!-- FOR IE9 below -->
 		<!--[if lt IE 9]>
-		<script src="js/respond.min.js"></script>
+		<script src="/js/respond.min.js"></script>
 		<![endif]-->
 
 	</head>
@@ -212,19 +212,19 @@ require_once 'translate.php';
 		</div>
 
 		<!-- jQuery -->
-		<script src="js/jquery.min.js"></script>
+		<script src="/js/jquery.min.js"></script>
 		<!-- jQuery Easing -->
-		<script src="js/jquery.easing.1.3.js"></script>
+		<script src="/js/jquery.easing.1.3.js"></script>
 		<!-- Bootstrap -->
-		<script src="js/bootstrap.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
 		<!-- Waypoints -->
-		<script src="js/jquery.waypoints.min.js"></script>
+		<script src="/js/jquery.waypoints.min.js"></script>
 		<!-- Counters -->
-		<script src="js/jquery.countTo.js"></script>
+		<script src="/js/jquery.countTo.js"></script>
 		<!-- Flexslider -->
-		<script src="js/jquery.flexslider-min.js"></script>
+		<script src="/js/jquery.flexslider-min.js"></script>
 
 		<!-- Main JS (Do not remove) -->
-		<script src="js/main.js"></script>
+		<script src="/js/main.js"></script>
 	</body>
 </html>
