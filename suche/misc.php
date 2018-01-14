e386d28ce799de8a8f169effed74a6781709413f
Johann150 <johann.galle@protonmail.com>
2018-01-14 11:37:55 +0100

    Signatur-Generierung angepasst

--- suche/misc.php
+++ suche/misc.php
@@ -4,12 +4,11 @@ require_once 'config.php';
 $con=dbCon();
 
 function formatSig($sig){
-	$sig=str_pad($sig,6,'0',STR_PAD_LEFT);
-	return substr($sig,0,2).'-'.substr($sig,2,2).'-'.substr($sig,4);
+	return preg_replace('/([A-Z]{3})([A-Z])([0-9]{3})/','\1-\2-\3',$sig);
 }
 
 function stripSig($str){
-	return preg_replace('/(\d\d)-(\d\d)-(\d\d)/',"\\1\\2\\3",$str);
+	return preg_replace('/([A-Z]{3})-([A-Z])-([0-9]{3})/','\1\2\3',$str);
 }
 
 function translate_iso639_2B($langcode){
