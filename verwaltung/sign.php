c8b11e190773952e298d2d3abaad08c581d7e476
Johann150 <johann.galle@protonmail.com>
2018-01-14 13:46:54 +0100

    kleines Hilfsscript
    dies sollte nicht im Produktivsystem ausgeführt werdem können, deswegen das exit(0) am Anfang
    Dies weist alle Signaturen neu zu, deshalb lokal machen und dann die SQL-Änderungen auf den Server packen

---
+++ verwaltung/sign.php
@@ -0,0 +1,29 @@
+<?php
+// aus Sicherheitsgründen sollte diese Datei nicht ausführbar sein
+// hiermit könnte eine "Denial of Service"-Atacke ausgeführt werden
+exit(0);
+
+header("Content-Type: text/plain");
+require_once "../suche/config.php";
+$pdo=dbConPDO();
+$stmnt=$pdo->prepare('SELECT row FROM (SELECT @row := @row + 1 as row,Titel FROM archivalien, (SELECT @row:=0)r WHERE Komponist=? ORDER BY Titel) AS a WHERE Titel=?');
+$upd_stmnt=$pdo->prepare('UPDATE archivalien SET Signatur = ? WHERE ID = ?');
+$unbekannt=0;
+foreach($pdo->query('SELECT ID,Titel,Komponist,Abk,Typus FROM archivalien LEFT JOIN komponisten ON Komponist=Name ORDER BY Titel') as $dsatz){
+	if(empty($dsatz['Abk'])){
+		$dsatz['Abk']="UNB";
+	}else{
+	}
+	$sig=$dsatz['Abk'].substr($dsatz['Typus'],0,1);
+	if($dsatz['Abk']=='UNB'){
+		$n=++$unbekannt;
+	}else{
+		$stmnt->execute(array($dsatz['Komponist'],$dsatz['Titel']));
+		$n=$stmnt->fetchColumn(0);
+	}
+	$sig.=str_pad($n,3,'0',STR_PAD_LEFT);
+	echo $dsatz['Titel'].' '.$sig."\n";
+	$upd_stmnt->execute(array($sig,$dsatz['ID']));
+	echo "-----------------------------------\n";
+}
+?>
