cb8f76424786b4497d6f909820dbb64e9094321e
vonMitzscha <marc.mitzscherling@protonmail.com>
2018-01-15 12:00:01 +0100

    #84

--- suche/details.php
+++ suche/details.php
@@ -102,7 +102,7 @@ require_once "../translate.php";
 					<div class="fh5co-navbar-brand">
 						<div class="row">
 							<div class="col-xs-6">
-								<h1 class="text-left"><a class="fh5co-logo" href="index.php"><span>Archiv</span> Handschriftenarchiv <span>Dresdner Kreuzchor</span></a></h1>
+								<h1 class="text-left"><a class="fh5co-logo" href="/<?php echo $use_lang; ?>/index.php"><span>Archiv</span> Handschriftenarchiv <span>Dresdner Kreuzchor</span></a></h1>
 							</div>
 							<div class="col-xs-6">
 								<!--
--- suche/search.php
+++ suche/search.php
@@ -63,65 +63,40 @@ if(isset($_GET['js'])){
 	  	<meta name="twitter:card" content="" />
 
 	  	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	  	<link rel="shortcut icon" href="../favicon.ico">
+	  	<link rel="shortcut icon" href="/favicon.ico">
 
 	  	<link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
 	  	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
 
 	  	<!-- Animate.css -->
-	  	<link rel="stylesheet" href="../css/animate.css">
+	  	<link rel="stylesheet" href="/css/animate.css">
 	  	<!-- Icomoon Icon Fonts-->
-	  	<link rel="stylesheet" href="../css/icomoon.css">
+	  	<link rel="stylesheet" href="/css/icomoon.css">
 	  	<!-- Bootstrap  -->
-	  	<link rel="stylesheet" href="../css/bootstrap.css">
+	  	<link rel="stylesheet" href="/css/bootstrap.css">
 
 	  	<!-- Flexslider  -->
-	  	<link rel="stylesheet" href="../css/flexslider.css">
+	  	<link rel="stylesheet" href="/css/flexslider.css">
 
-	  	<link rel="stylesheet" href="../css/style.css">
+	  	<link rel="stylesheet" href="/css/style.css">
 
 
 	  	<!-- Modernizr JS -->
-	  	<script src="../js/modernizr-2.6.2.min.js"></script>
+	  	<script src="/js/modernizr-2.6.2.min.js"></script>
 	  	<!-- FOR IE9 below -->
 	  	<!--[if lt IE 9]>
-	  	<script src="js/respond.min.js"></script>
+	  	<script src="/js/respond.min.js"></script>
 	  	<![endif]-->
 	</head>
 	<body>
-		<nav id="fh5co-main-nav" role="navigation">
-	 	 <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
-	 	 <div class="js-fullheight fh5co-table">
-	 		 <div class="fh5co-table-cell js-fullheight">
-	 			 <h1 class="text-center"><a class="fh5co-logo" href="index.php">Handschriftenarchiv Dresdner Kreuzchor</a></h1>
-	 			 <ul>
-	 				 <li><a href="../index.php">Willkommen</a></li>
-	 				 <li><a href="../archiv.php">Archiv</a></li>
-	 				 <li><a href="../blog.php">Blog</a></li>
-	 				 <li><a href="../mediathek.php">Mediathek</a></li>
-	 				 <li><a href="/suche/">Datenbank</a></li>
-	 				 <li><a href="../impressum.php">Impressum</a></li>
-	 				 <li><a href="../kontakt.php">Kontakt</a></li>
-	 			 </ul>
-	 			 <p class="fh5co-social-icon">
-	 				 <a href="https://twitter.com/NotenarchivDKC"target="_blank"><i class="icon-twitter2"></i></a>
-	 				 <a href="https://issuu.com/hsa6"target="_blank"><i class="icon-book"></i></a>
-	 				 <a href="https://vimeo.com/handschriftenarchiv"target="_blank"><i class="icon-vimeo"></i></a>
-	 				 <a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"target="_blank"><i class="icon-youtube"></i></a>
-	 				 <a href="https://plus.google.com/108785494716898198379"target="_blank"><i class="icon-google"></i></a>
-	 				 <a href="https://github.com/handschriftenarchiv"target="_blank"><i class="icon-github2"></i></a>
-	 				 <a href="https://be.net/handschriftenarchiv"target="_blank"><i class="icon-behance"></i></a>
-	 			 </p>
-	 		 </div>
-	 	 </div>
-	  </nav>
+		<?php include '../lang/nav.php';?>
 		<div id="fh5co-page">
 			<header>
 				<div class="container">
 					<div class="fh5co-navbar-brand">
 						<div class="row">
 							<div class="col-xs-6">
-								<h1 class="text-left"><a class="fh5co-logo" href="index.php"><span>Archiv</span> Handschriftenarchiv <span>Dresdner Kreuzchor</span></a></h1>
+								<h1 class="text-left"><a class="fh5co-logo" href="/<?php echo $use_lang; ?>/index.php"><span>Archiv</span> Handschriftenarchiv <span>Dresdner Kreuzchor</span></a></h1>
 							</div>
 							<div class="col-xs-6">
 								<!--
@@ -140,7 +115,7 @@ if(isset($_GET['js'])){
 		<aside id="fh5co-hero" class="js-fullheight">
 			<div class="flexslider js-fullheight">
 				<ul class="slides">
-				<li style="background-image: url(../images/database/start.jpg);">
+				<li style="background-image: url(/images/database/start.jpg);">
 					<div class="overlay-gradient"></div>
 					<div class="container">
 						<div class="col-md-8 col-md-offset-2 col-md-push-4 js-fullheight slider-text">
@@ -234,19 +209,19 @@ if(isset($_GET['search'])){
 
 		</div>
 		<!-- jQuery -->
-		<script src="../js/jquery.min.js"></script>
+		<script src="/js/jquery.min.js"></script>
 		<!-- jQuery Easing -->
-		<script src="../js/jquery.easing.1.3.js"></script>
+		<script src="/js/jquery.easing.1.3.js"></script>
 		<!-- Bootstrap -->
-		<script src="../js/bootstrap.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
 		<!-- Waypoints -->
-		<script src="../js/jquery.waypoints.min.js"></script>
+		<script src="/js/jquery.waypoints.min.js"></script>
 		<!-- Counters -->
-		<script src="../js/jquery.countTo.js"></script>
+		<script src="/js/jquery.countTo.js"></script>
 		<!-- Flexslider -->
-		<script src="../js/jquery.flexslider-min.js"></script>
+		<script src="/js/jquery.flexslider-min.js"></script>
 
 		<!-- Main JS (Do not remove) -->
-		<script src="../js/main.js"></script>
+		<script src="/js/main.js"></script>
 	</body>
 </html>
