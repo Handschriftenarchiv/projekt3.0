fc61ff19bab5d59c94dfa25de528d29efd7c68b4
Johann150 <johann.galle@protonmail.com>
2018-03-14 18:02:44 +0100

    IDL-script in nav.php

--- verwaltung/index.php
+++ verwaltung/index.php
@@ -67,63 +67,7 @@ require_once '../analytics.php';
 
  </head>
  <body>
-	 <script type="text/javascript">
-window._idl = {};
-_idl.variant = "modal";
-(function() {
-	 var idl = document.createElement('script');
-	 idl.async = true;
-	 idl.src = 'https://members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'modal');
-	 document.getElementsByTagName('body')[0].appendChild(idl);
-})();
-</script>
-<!--
-	 <nav id="fh5co-main-nav" role="navigation">
-		 <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
-		 <div class="js-fullheight fh5co-table">
-			 <div class="fh5co-table-cell js-fullheight">
-				 <h1 class="text-center"><a class="fh5co-logo" href="index.php">Sprache</a></h1>
-				 <ul>
-					 <li><a href="index.php">Deutsch</a></li>
-					 <li><a href="/en/index.php">Englisch</a></li>
-				 </ul>
-				 <p class="fh5co-social-icon">
-					 <a href="https://twitter.com/NotenarchivDKC"><i class="icon-twitter2"></i></a>
-
-					 <a href="https://issuu.com/hsa6"><i class="icon-book"></i></a>
-					 <a href="https://vimeo.com/handschriftenarchiv"><i class="icon-vimeo"></i></a>
-					 <a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"><i class="icon-youtube"></i></a>
-				 </p>
-			 </div>
-		 </div>
-	 </nav>
--->
- <nav id="fh5co-main-nav" role="navigation">
-	 <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle active"><i></i></a>
-	 <div class="js-fullheight fh5co-table">
-		 <div class="fh5co-table-cell js-fullheight">
-			 <h1 class="text-center"><a class="fh5co-logo" href="../index.php">Handschriftenarchiv Dresdner Kreuzchor</a></h1>
-			 <ul>
-				 <li><a href="../index">Willkommen</a></li>
-				 <li><a href="../archiv">Archiv</a></li>
-				 <li><a href="../blog/">Blog</a></li>
-				 <li><a href="../mediathek">Mediathek</a></li>
-				 <li><a href="../suche/">Datenbank</a></li>
-				 <li><a href="../impressum">Impressum</a></li>
-				 <li><a href="../kontakt">Kontakt</a></li>
-			 </ul>
-			 <p class="fh5co-social-icon">
-				 <a href="https://twitter.com/NotenarchivDKC"target="_blank"><i class="icon-twitter2"></i></a>
-				 <a href="https://issuu.com/hsa6"target="_blank"><i class="icon-book"></i></a>
-				 <a href="https://vimeo.com/handschriftenarchiv"target="_blank"><i class="icon-vimeo"></i></a>
-				 <a href="https://www.youtube.com/channel/UCLuX1DzvPkx1OBjjuKQhXPw"target="_blank"><i class="icon-youtube"></i></a>
-				 <a href="https://plus.google.com/108785494716898198379"target="_blank"><i class="icon-google"></i></a>
-				 <a href="https://github.com/handschriftenarchiv"target="_blank"><i class="icon-github2"></i></a>
-				 <a href="https://be.net/handschriftenarchiv"target="_blank"><i class="icon-behance"></i></a>
-			 </p>
-		 </div>
-	 </div>
- </nav>
+		<?php include "../lang/nav.php"; ?>
 
  <div id="fh5co-page">
 	 <header>
