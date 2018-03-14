c3470b8f4b6a9af70cdc059acfd407a756e45aed
Johann150 <johann.galle@protonmail.com>
2018-03-14 18:19:17 +0100

    Links berichtigt
    resolves #84

--- blog/blog.php
+++ blog/blog.php
@@ -89,7 +89,7 @@ $next=$data[$index]['title'];
 					<div class="fh5co-navbar-brand">
 						<div class="row">
 							<div class="col-xs-6">
-								<h1 class="text-left"><a class="fh5co-logo" href="../index.php"><span>Archiv</span> Handschriftenarchiv <span>Dresdner Kreuzchor</span></a></h1>
+								<h1 class="text-left"><a class="fh5co-logo" href="/<?php echo $use_lang; ?>/"><span>Archiv</span> Handschriftenarchiv <span>Dresdner Kreuzchor</span></a></h1>
 							</div>
 							<div class="col-xs-6">
 								<!--<p class="fh5co-social-icon text-right">
