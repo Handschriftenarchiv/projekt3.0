7b063363bda39ead59208c8735daeab8ffbf1c4d
Johann150 <johann.galle@protonmail.com>
2022-03-04 22:09:47 +0100

    Refactoring Analytics-Anzeige
    Nie mehr als die Daten von 1 Jahr anzeigen, um Browser nicht zu überfordern.
    
    PHP-Code vereinfacht und aufgeräumt.

--- verwaltung/graphs.php
+++ verwaltung/graphs.php
@@ -1,5 +1,98 @@
 <?php
 require_once '../analytics.php';
+
+$con=dbCon();
+
+if(isset($_GET['archivalien'])){
+	$type = 'archivalien';
+	$series_sql="SELECT Signatur as additional FROM analytics JOIN archivalien ON additional = archivalien.ID "
+		."WHERE page like '%details.php' "
+		."AND created > CURDATE() - INTERVAL 1 YEAR GROUP BY Signatur";
+	$data_sql = "SELECT DATE(created) AS dateCreated,COUNT(*) AS views,Signatur as additional FROM analytics "
+		."JOIN archivalien ON additional = archivalien.ID "
+		."WHERE page like '%details.php' AND created > CURDATE() - INTERVAL 1 YEAR "
+		."GROUP BY additional,dateCreated ORDER BY dateCreated,additional ASC";
+}elseif(isset($_GET['notfound'])){
+	$type = 'notfound';
+	$series_sql="SELECT additional FROM analytics WHERE page like '%not-found.php' "
+		."AND created > CURDATE() - INTERVAL 1 YEAR GROUP BY additional";
+	$data_sql="SELECT DATE(created) AS dateCreated,COUNT(*) AS views,additional FROM analytics "
+		."WHERE page like '%not-found.php' AND created > CURDATE() - INTERVAL 1 YEAR "
+		."GROUP BY additional,dateCreated ORDER BY dateCreated,additional ASC";
+}elseif(isset($_GET['blog'])){
+	$type = 'blog';
+	$series_sql="SELECT additional FROM analytics WHERE page like '%blog.php' "
+		."AND created > CURDATE() - INTERVAL 1 YEAR GROUP BY additional";
+	$data_sql="SELECT DATE(created) AS dateCreated,COUNT(*) AS views,additional FROM analytics "
+		."WHERE page like '%blog.php' AND created > CURDATE() - INTERVAL 1 YEAR "
+		."GROUP BY additional,dateCreated ORDER BY dateCreated,additional ASC";
+}elseif(isset($_GET['komponist'])){
+	$type = 'komponist';
+	$series_sql="SELECT Name as additional FROM analytics LEFT JOIN komponisten ON additional = Abk WHERE page like '%komponist-info.php' "
+		."AND created > CURDATE() - INTERVAL 1 YEAR GROUP BY additional";
+	$data_sql="SELECT DATE(created) as dateCreated,COUNT(*) AS views,Name as additional FROM analytics "
+		."LEFT JOIN komponisten ON analytics.additional = komponisten.Abk WHERE page like '%komponist-info.php' "
+		."AND created > CURDATE() - INTERVAL 1 YEAR GROUP BY additional,dateCreated ORDER BY dateCreated,additional ASC";
+}else{
+	$type = 'default';
+	$series_sql="SELECT page as additional FROM analytics WHERE created > CURDATE() - INTERVAL 1 YEAR "
+		."GROUP BY page";
+	$data_sql="SELECT DATE(created) AS dateCreated,COUNT(*) AS views,page as additional FROM analytics "
+		."WHERE created > CURDATE() - INTERVAL 1 YEAR "
+		."GROUP BY page,dateCreated ORDER BY dateCreated,page ASC";
+}
+
+$res_data=mysqli_query($con,$data_sql);
+$count=mysqli_num_rows($res_data);
+
+if($count>0){
+	// Daten laden
+	$con=dbCon();
+
+	// Kategorien laden
+	$res_series=mysqli_query($con,$series_sql);
+	$series=array();
+	while($dsatz=mysqli_fetch_assoc($res_series)){
+		$series[] = $dsatz['additional'];
+	}
+
+	// eigentliche Daten laden
+
+	/*
+	$res_data enthält Daten gruppiert nach Tag und "Kategorie" (z.B. Archivalie)
+	Nun müssen Daten nach Datum je in eine Zeile zusammengefasst werden. Dazu
+	Gruppierung nun abbilden als mehrdimensionales Array
+	1. Index: Datum
+	2. Index: Kategorie
+	*/
+	$data=array();
+	while($dsatz=mysqli_fetch_assoc($res_data)){
+		$data[date('j/n/y',strtotime($dsatz['dateCreated']))][$dsatz['additional']]=$dsatz['views'];
+	}
+
+	$csv='Tag,"'.implode('","',$series)."\"\n";
+	foreach($data as $date => $views) {
+		$csv.=$date.',';
+		foreach($series as $s) {
+			if(isset($views[$s])) {
+				$csv.=$views[$s].',';
+			}else{
+				$csv.='0,';
+			}
+		}
+		$csv.="\n";
+	}
+
+	// series in entsprechende JSON-Objekte umwandeln
+	$series_obj = array();
+	foreach($series as $s) {
+		$series_obj[] = array('name' => $s);
+	}
+	$series = json_encode($series_obj);
+
+	mysqli_close($con);
+}
+
 ?><!DOCTYPE html>
 <html>
 	<head>
@@ -44,9 +137,9 @@ require_once '../analytics.php';
 		</style>
 	</head>
 	<body>
-		<?php if(hasData()){ ?>
-		<script src="highcharts.js"></script>
-		<script src="highcharts-data.js"></script>
+<?php if($count>0){ ?>
+		<script src="highcharts.js" type="application/javascript"></script>
+		<script src="highcharts-data.js" type="application/javascript"></script>
 
 		<div id="chart" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 		<link href="/fonts/karla/include.css" rel="stylesheet"/>
@@ -57,7 +150,7 @@ require_once '../analytics.php';
 					zoomType: 'x'
 				},
 				data:{
-					csv: "<?php echoData(); ?>"
+					csv: <?php echo json_encode($csv); ?>
 				},
 				title: {
 					text: 'Zugriffe pro Tag',
@@ -116,25 +209,31 @@ require_once '../analytics.php';
 						}
 					}
 				},
-				series: <?php echoSeries(); ?>
+				series: <?php echo $series; ?>
 			});
 		</script>
-	<?php }else{ ?>
+<?php }else{ ?>
 		<h3>keine Daten</h3>
-	<?php } ?>
+<?php } ?>
 		<center>
 			<p>
 				<a href="/<?php echo $use_lang; ?>/">Zurück zur Startseite</a><br>
 				<a href=".">Zurück zum Verwaltungsportal</a><br>
-				<?php if(isset($_GET['archivalien'])||isset($_GET['notfound'])){ ?>
+<?php if($type!=='default'){ ?>
 				<a href="graphs">Seitenzugriffe</a><br>
-				<?php }
-				if(!isset($_GET['notfound'])){ ?>
+<?php }
+if($type!=='notfound'){ ?>
 				<a href="graphs?notfound">nicht gefundene Dokumente</a><br>
-				<?php }
-				if(!isset($_GET['archivalien'])){ ?>
-				<a href="graphs?archivalien">Zugriffe auf den Katalog</a>
-				<?php } ?>
+<?php }
+if($type!=='archivalien'){ ?>
+				<a href="graphs?archivalien">Zugriffe auf den Katalog</a><br>
+<?php }
+if($type!=='blog'){ ?>
+				<a href="graphs?notfound">Zugriffe auf den Blog</a><br>
+<?php }
+if($type!=='komponist'){ ?>
+				<a href="graphs?notfound">Zugriffe auf Komponisten-Informationen</a><br>
+<?php } ?>
 			</p>
 		</center>
 	</body>
